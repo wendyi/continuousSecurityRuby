481b5df310512527ad9c9d299fa31cc0bdf17d7d
Wendy Istvanick <wendyistvanick@wendy-istvanick.local>
2016-03-23 16:19:50 -0500

    Changing talisman check to use the version of talisman in /usr/local/bin.

---
+++ talisman.sh
@@ -0,0 +1,6 @@
+printf "refs/heads/master/ " 
+lastCommit="$(git rev-parse HEAD)"
+printf $lastCommit
+printf " refs/heads/master/ "
+firstCommit="$(git rev-list HEAD | tail -n 1)"
+printf $firstCommit
