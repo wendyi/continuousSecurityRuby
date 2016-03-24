b1b935a2f96a7783e58a45f6c893430407dc9a8d
Wendy Istvanick <wendyistvanick@wendy-istvanick.local>
2016-03-24 10:49:43 -0500

    Removing talisman.sh to add it again with the right permisions.

--- talisman.sh
+++
@@ -1,7 +0,0 @@
-printf "refs/heads/master/ " 
-lastCommit="$(git rev-parse HEAD)"
-printf $lastCommit
-printf " refs/heads/master/ "
-firstCommit="$(git rev-list HEAD | tail -n 1)"
-printf $firstCommit
-
